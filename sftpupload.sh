94b02c564150958be9a407d8a7cf6d43128802b0
Nando Paz Pereira <31595040+nandopazpereira@users.noreply.github.com>
2022-10-12 14:47:54 +0200

    Update sftpupload.sh

--- sftpupload.sh
+++ sftpupload.sh
@@ -2,7 +2,7 @@
                             ######### Written by N0z ##########
 # This script is part of N0z's linux maintanance repo
 # For this to work, you need to have expect installed on your system.
-#Remember to escape any special characters in your password, as bash might interpret it differently, causing sign-in failure!
+# Remember to escape any special characters in your password, as bash might interpret it differently, causing sign-in failure!
  
 spawn sftp User@your_server_ip
 expect "password:"
